31c0505f521bb9fdcda599d12021c146fdf496c2
larrykirschner <larrykirschner@gmail.com>
2017-08-21 22:58:00 -0700

    0.0.1

---
+++ src/CollectionExtensions/CollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+
+namespace BeatThat
+{
+	public static class CollectionExtensions
+	{
+		public static void AddRange<T>(this ICollection<T> toC, ICollection<T> fromC)
+		{
+			var asList = toC as List<T>;
+			if(asList != null) {
+				asList.AddRange(fromC);
+				return;
+			}
+
+			foreach(var o in fromC) {
+				toC.Add(o);
+			}
+		}
+	}
+}
