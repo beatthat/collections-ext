a778670494e4e6f93176e3fc5af0f2e3c6433825
larrykirschner <larrykirschner@gmail.com>
2017-09-08 18:14:42 -0700

    0.0.2

--- src/collections-ext/CollectionsExt.cs
+++ src/collections-ext/CollectionsExt.cs
@@ -5,6 +5,12 @@ namespace BeatThat
 {
 	public static class CollectionExtensions
 	{
+		/// <summary>
+		/// Enable <c>ICollection.AddRange<T></c> which is normally only available on <c>IList<T></c>
+		/// </summary>
+		/// <param name="toC">To c.</param>
+		/// <param name="fromC">From c.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
 		public static void AddRange<T>(this ICollection<T> toC, ICollection<T> fromC)
 		{
 			var asList = toC as List<T>;
