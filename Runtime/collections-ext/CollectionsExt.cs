feae2093479f05c32b3f3b84618f65c14a000d99
Larry Kirschner <larrykirschner@gmail.com>
2018-06-03 11:18:22 -0700

    updated template

---
+++ Runtime/collections-ext/CollectionsExt.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+
+
+namespace BeatThat
+{
+	public static class CollectionExtensions
+	{
+		/// <summary>
+		/// Enable <c>ICollection.AddRange<T></c> which is normally only available on <c>IList<T></c>
+		/// </summary>
+		/// <param name="toC">To c.</param>
+		/// <param name="fromC">From c.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static void AddRange<T>(this ICollection<T> toC, ICollection<T> fromC)
+		{
+			var asList = toC as List<T>;
+			if(asList != null) {
+				asList.AddRange(fromC);
+				return;
+			}
+
+			foreach(var o in fromC) {
+				toC.Add(o);
+			}
+		}
+
+		public static bool AddIfMissing<T>(this ICollection<T> c, T elem)
+		{
+			if (c.Contains (elem)) {
+				return false;
+			}
+			c.Add (elem);
+			return true;
+		}
+	}
+}
